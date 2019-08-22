86f22a57c7f49f91a4ad08af59d45454f0e2ab81
jpdrcarneiro <jpdrcarneiro@yahoo.com>
2019-08-22 14:52:57 -0700

    New class and release

---
+++ MonkeyJobNodes/miscellaneous.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyJobNodes
+{
+   public static class Miscellaneous
+   {
+      /// <summary>
+      /// Check if a string is empty or not. Empty string == false; Use this to generate a boolean mask;
+      /// </summary>
+      /// <param name="stringCont"></param>
+      /// <returns name="result">Returns false if the string is empty</returns>
+      public static bool isStringEmpty(string stringCont)
+      {
+         if (stringCont.Length > 1)
+         {
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+
+      }
+
+
+   }
+}
