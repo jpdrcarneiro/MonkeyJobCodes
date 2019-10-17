11961fbda5e5fcba2bb7c2c4b6531ee87590924f
jpdrcarneiro <jcarneir@usc.edu>
2019-10-17 13:42:00 -0700

    Navis Plugin Test

--- MonkeyJobNavis/MonkeyJobsNavisPlugIn/MonkeyJobsNavisPlugIn/MonkeyJobsPlugin.cs
+++ MonkeyJobNavis/MonkeyJobsNavisPlugIn/MonkeyJobsNavisPlugIn/MonkeyJobsPlugin.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+//Navisworks References
+using Autodesk.Navisworks.Api;
 using Autodesk.Navisworks.Api.Plugins;
+
+//External References
+using IronPdf;
 
 namespace MonkeyJobsNavisPlugIn
 {
-    [Plugin("MonkeyJobsNavis", "jpdrcarneiro", DisplayName = "Monkey Jobs")]
-    public class MonkeyJobsPlugin:CommandHandlerPlugin
-    {
+    [PluginAttribute("MonkeyJobs.Generate PDF Report",                   //Plugin name
+                    "MNKJ",                                       //4 character Developer ID or GUID
+                    ToolTip = "It converts Clash tests into a PDF Report",//The tooltip for the item in the ribbon
+                    DisplayName = "Generate PDF Report")]          //Display name for the Plugin in the Ribbon
 
-        public override int ExecuteCommand(string name, params string[] parameters)
+    public class GeneratePDFReport : AddInPlugin
+    {
+        public override int Execute(params string[] parameters)
         {
+            MessageBox.Show(Autodesk.Navisworks.Api.Application.Gui.MainWindow, "Generate PDF Success!!!");
             return 0;
         }
     }
