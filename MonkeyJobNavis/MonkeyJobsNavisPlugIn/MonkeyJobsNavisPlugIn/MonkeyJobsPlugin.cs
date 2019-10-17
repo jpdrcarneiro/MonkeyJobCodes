15d302f837e2601336a9e4418929b343a321ed27
jpdrcarneiro <jcarneir@usc.edu>
2019-10-16 17:03:24 -0700

    Navisworks Plugin

---
+++ MonkeyJobNavis/MonkeyJobsNavisPlugIn/MonkeyJobsNavisPlugIn/MonkeyJobsPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace MonkeyJobsNavisPlugIn
+{
+    [Plugin("MonkeyJobsNavis", "jpdrcarneiro", DisplayName = "Monkey Jobs")]
+    public class MonkeyJobsPlugin:CommandHandlerPlugin
+    {
+
+        public override int ExecuteCommand(string name, params string[] parameters)
+        {
+            return 0;
+        }
+    }
+}
